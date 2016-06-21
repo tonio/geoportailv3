00ef171dbb6348511535e8a0fe308e8bb6d7cb68
Antoine Abt <antoine@abt.im>
2016-06-21 15:29:22 +0200

    Add mouse position control config (& style closer to main app)

--- jsapi/examples/simple.js
+++ jsapi/examples/simple.js
@@ -3,10 +3,12 @@ goog.provide('index');
 goog.require('lux');
 
 var map = new lux.Map({
-  target      : 'mapContainer',
-  position    : [ 6.13, 49.61 ],
-  positionSrs : '4326',
-  zoom        : 14,
-  bgLayer     : 'streets_jpeg',
-  layers      : [ '543' ]
+  target           : 'mapContainer',
+  position         : [ 6.13, 49.61 ],
+  positionSrs      : '4326',
+  zoom             : 14,
+  bgLayer          : 'streets_jpeg',
+  layers           : [ '543' ],
+  mousePosition    : true,
+  mousePositionSrs : 4326
 });
